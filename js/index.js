a8c6009ecde1c024b81d5873c65b57d436d19d92
Eryy-crypz <irfyxnz@gmail.com>
2026-03-11 05:03:28 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Qaseh?cuba bukak nii`.split('')
+const text = `Qaseh?cuba bukak ni`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
